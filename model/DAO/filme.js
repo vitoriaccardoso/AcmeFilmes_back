7584b7c8998e404b85de1f3d56f34e2d1f0bae7e
VITÓRIA CARDOSO COSTA <23111831@CFP127ALUNO.LOCAL>
2024-02-22 17:12:32 -0300

    fazendo

--- model/DAO/filme.js
+++ model/DAO/filme.js
@@ -7,6 +7,20 @@
  * 
  */
 
+
+
+//Import da biblioteca do prisma client
+const { PrismaClient } = require ('@prisma/client')
+
+
+
+//Instância da classe prisma client 
+const prisma = new PrismaClient()
+
+
+
+
+
 //Função para inserir novo filme no Banco de Dados
 const insertFilme = async function(){
 
@@ -25,12 +39,58 @@ const deleteFilme = async function(){
 //Função para listar todos os filme do banco de dados
 const selectAllFilme = async function(){
 
+
+    let sql = 'select * from tbl_filme'
+
+    //$queryRawUnsafe(sql)
+    //$queryRaw('select * from tbl_filme')
+
+    let rsFilmes = await prisma.$queryRawUnsafe(sql)
+
+    if(rsFilmes.length > 0)
+        return rsFilmes
+    else 
+        return false
+
+
 }
 
 //Função para buscar um filme do Banco de Dados pelo ID
-const selectByIdFilme = async function(){
+const selectByIdFilme = async function(id){
+
+    //encaminha o script sql par o bd
+    try {
+        let sql = `select * from tbl_filme where id = ${id}`
+    
+        let rsFilme = await prisma.$queryRawUnsafe(sql)
+        return rsFilme
+        
+    } catch (error) {
+        return false
+        
+    }
 
 }
+
+const selectByNomeFilme = async function(nome){
+    try {
+        let sql = `select * from tbl_filme where nome like '%${nome}%'`
+
+        let rsFilme = await prisma.$queryRawUnsafe(sql)
+
+        return rsFilme
+        
+    } catch (error) {
+
+        return false
+        
+    }
+
+
+}
+
+//Função para buscar um filme do Banco de Dados pelo nome
+
 
 module.exports = {
     insertFilme,
@@ -38,5 +98,6 @@ module.exports = {
     deleteFilme,
     selectAllFilme,
     selectAllFilme,
-    selectByIdFilme
+    selectByIdFilme,
+    selectByNomeFilme
 }
